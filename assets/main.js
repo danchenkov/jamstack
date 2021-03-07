ded1db4fdf2784f8237fbb2c8fcd07a49f396116
Alexei Danchenkov <alexeid@scientology.net>
2021-03-06 17:12:22 -0800

    partials

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,9 @@
-console.log("JS log")
+console.log("Logger")
+
+function f(arr) {
+    let aim = arr.filter(v=>v%2===1).map(v=>v**2)
+    let af = aim.reduce((acc,v)=>acc+v,0)
+    return af
+}
+
+console.log(f([2,3,4,5]))
